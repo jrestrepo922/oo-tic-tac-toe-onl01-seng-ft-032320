65e17b00ba296db22e6c0d40476b8e80ed61502d
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-03 13:27:54 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,120 @@
+class TicTacToe 
+  
+  WIN_COMBINATIONS = [
+      [0, 1, 2], # horizontal winning combinations
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6], # vertical winning combinations
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4 ,8],
+      [2, 4, 6]
+    ]
+  
+  
+  
+  def initialize(board = nil)
+    # if board is nil than is false and the new array gets created
+    @board = board || Array.new(9, " ")
+  end 
+  
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(input)
+    input_to_number = input.to_i 
+    index = input_to_number - 1
+  end 
+  
+  def move(index, token = "X")
+    @board[index] = token
+  end 
+  
+  def position_taken?(index)
+    if (@board[index] == nil || @board[index] == "" || @board[index] == " ")
+      false 
+    else
+      true
+    end 
+  end 
+  
+  def valid_move?(index)
+    if(!position_taken?(index) && index.between?(0, 8))
+      true 
+    else 
+      false
+    end 
+  end 
+  
+  
+  def turn_count
+    @board.count { |position|
+      position.include?("X") || position.include?("O")
+    }
+  end 
+  
+  
+  def current_player
+    if (turn_count % 2 == 0)
+      "O"
+    else 
+      "X"
+    end 
+  end 
+  
+  
+  def turn
+    puts "Please choose a poistion between 1-9"
+    input = gets.strip 
+    index = input_to_index(input)
+    
+    if valid_move?(index)
+      move(index)
+      display_board
+    else
+      turn
+    end
+    
+    
+  
+  end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
